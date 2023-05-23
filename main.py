407e085b157d3043abde86c145aeb4c5556d627d
Elliott Stenvall <elst1127@storsjogymnasiet.se>
2023-05-23 09:09:55 +0200

    sebbe commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single
+import os
+import Jokehandler
+
+def main():
+    print("Hej")
+
+
+main()
